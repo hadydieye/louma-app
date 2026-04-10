9a09fd51debad47ac01a481dc010c83accd56dfb
hadydieye <dieyejunior6@gmail.com>
2026-04-10 13:51:07 +0000

    fix: update supabase.ts

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -1,15 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
-import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-        storage: AsyncStorage,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: false,
-    },
+  auth: {
+    storage: Platform.OS !== 'web' ? AsyncStorage : undefined,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
 });
